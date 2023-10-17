9937a6e6919251e445aad7f5782e7cf8fcad93f6
rnahill <rei333lly@gmail.com>
2023-10-17 18:22:37 -0400

    changes to css and html

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,8 @@
 // function for checking for correct answers
 
 // variables needed
-// 
+// question 1 correct answer
+// question 2 correct answer
+// question 3 correct answer
+// question 4 correct answer
+// question 5 correct answer
