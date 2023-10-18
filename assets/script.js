adb66abbda629b8232bbaa231c522cb82e824ee9
rnahill <rei333lly@gmail.com>
2023-10-18 11:14:18 -0400

    created javascript variables and organized id's

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,61 @@
-// function for displaying questions
-// timer function
-// function for checking for correct answers
+/* ID's and CLASSES THAT DO NOT NEED CSS? */
 
-// variables needed
-// question 1 correct answer
-// question 2 correct answer
-// question 3 correct answer
-// question 4 correct answer
-// question 5 correct answer
+/*  */
+/* id openingSection */
+/* id intro */
+/* id startBtn */
+
+/* id quizSection */
+/* id timer */
+/* id question */
+// id choices
+/* class choiceBtn */
+/* id's choice1, 2, 3, 4 */
+
+/* id endingSection */
+/* id score */
+/* id initials */
+/* id saveBtn */
+
+/* id scoresSection */
+/* id highScores */
+/* id restartBtn */
+/* id clearBtn */
+
+
+// PSEUDOCODE
+// create variables
+// create array for questions and answers
+// create function for timer
+// create function for displaying questions
+// create function for saving a score
+// create function for displaying all highscores
+
+var openingEL = document.getElementById("openingSection");
+var introEL = document.getElementById("intro");
+var startBtnEL = document.getElementById("startBtn");
+
+var quizEL = document.getElementById("quizSection");
+var timerEl = document.getElementById("timer");
+var questionEl = document.getElementById("question");
+var choicesEl = document.getElementById("choices");
+// variables for choice1, 2, 3, 4?
+
+var endingEL = document.getElementById("endingSection");
+var scoreEl = document.getElementById("score");
+var initialsEl = document.getElementById("initials");
+var saveBtnEl = document.getElementById("saveBtn");
+
+var scoresEL = document.getElementById("scoresSection");
+var highScoresEl = document.getElementById("highScores");
+var restartBtnEl = document.getElementById("restartBtn");
+var clearBtnEl = document.getElementById("clearBtn");
+
+let score = 0;
+let timeLeft = 60;
+
+function countDown() {
+    timerEl.textContent = "Time left: " + timeLeft;
+
+
+}
