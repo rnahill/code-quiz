88d7c573dc62dd4ac1e8ed2c7db2cd877a7d420a
rnahill <rei333lly@gmail.com>
2023-10-19 16:40:56 -0400

    clean up

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,3 @@
-/* ID's and CLASSES THAT DO NOT NEED CSS? */
-
-/*  */
-/* id openingSection */
-/* id intro */
-/* id startBtn */
-
-/* id quizSection */
-/* id timer */
-/* id question */
-// id choices
-/* class choiceBtn */
-/* id's choice1, 2, 3, 4 */
-
-/* id endingSection */
-/* id score */
-/* id initials */
-/* id saveBtn */
-
-/* id scoresSection */
-/* id highScores */
-/* id restartBtn */
-/* id clearBtn */
-
-
-// PSEUDOCODE
-// create variables
-// create array for questions and answers
-// create function for timer
-// create function for displaying questions
-// create function for saving a score
-// create function for displaying all highscores
-
 var openingEL = document.getElementById("openingSection");
 var introEL = document.getElementById("intro");
 var startBtnEL = document.getElementById("startBtn");
